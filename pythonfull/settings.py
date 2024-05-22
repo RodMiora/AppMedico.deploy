5d6dbd8b5b4e52ae64fc3b00d71032575f2b3ea7
RodMiora <rodrigomiorin@gmail.com>
2024-05-22 02:29:54 -0300

    Update settings.py

--- pythonfull/settings.py
+++ pythonfull/settings.py
@@ -8,10 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-5iqgjs$a!e%g^x9uhr%906bzx__y-o_k%(pq=pi+8uhck2bhpz"
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
